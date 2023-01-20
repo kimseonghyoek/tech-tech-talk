8870eb57fc8436fbe7af02da81b814b8129d2fc8
Kimseonghyoek <cilo1223@naver.com>
2023-01-20 22:35:02 +0900

    Update App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Input from './components/Input';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Routes } from 'react-router';
+import Promotion from './pages/Promotion';
 
 function App() {
   return (
     <div className="App">
-      <Input placeholder='Custom Input Components' onChange={null}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Promotion/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ front/src/pages/Promotion.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Promotion() {
+  return (
+    <div>
+      <h1>First Page</h1>
+    </div>
+  )
+}
+
+export default Promotion;
