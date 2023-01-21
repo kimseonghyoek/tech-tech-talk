c21a13c744e484431e40c66c9a3addbac1d3a210
Kimseonghyoek <cilo1223@naver.com>
2023-01-21 23:46:26 +0900

    Update Login Page background img

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -1,1 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import palette from '../palette';
+
+const public_url = process.env.PUBLIC_URL;
+
+const Container = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    background-image: url(${public_url}/imgs/login_background.jpg);
+    background-size: cover; 
+`;
+
+function Login() {
+  return (
+    <Container>
+    </Container>
+  )
+}
+
+export default Login;
