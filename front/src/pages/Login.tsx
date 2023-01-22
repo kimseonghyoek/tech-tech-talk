cd73cfc54f82a47bc8c432596661a192d1146614
Kimseonghyoek <cilo1223@naver.com>
2023-01-23 03:27:34 +0900

    Update Login page

--- front/src/pages/Login.tsx
+++ front/src/pages/Login.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import styled from 'styled-components';
 import LoginForm from '../components/LoginForm';
 
-const public_url = process.env.PUBLIC_URL;
-
 const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     width: 100%;
     height: 100vh;
-    background-image: url(${public_url}/imgs/login_background.jpg);
-    background-size: cover; 
 `;
 
 function Login() {
