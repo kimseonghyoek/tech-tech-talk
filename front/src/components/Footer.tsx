3194b295b55d45e439c62d15983f5b706d76dc5b
Kimseonghyoek <cilo1223@naver.com>
2023-01-23 03:25:24 +0900

    Create Footer Component

---
+++ front/src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import palette from "../palette";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 14rem;
+  background-color: ${palette.main_color2};
+
+  h1 {
+    text-align: center;
+  }
+
+  p {
+    text-align: right;
+  }
+`;
+
+function Footer(): JSX.Element {
+  return (
+    <Container>
+      <div>
+        <h1>Tech-Tech-Talk - Dev 2023 ~ </h1>
+        <p>Develop by Overloper</p>
+      </div>
+    </Container>
+  );
+}
+
+export default Footer;
