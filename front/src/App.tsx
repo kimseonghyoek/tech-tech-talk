51645535c3f3d5bcbd4d9cf78b19a8fd92613dbb
Kimseonghyoek <cilo1223@naver.com>
2023-01-23 03:28:51 +0900

    Update App system & change style

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
 import Promotion from './pages/Promotion';
 import Login from './pages/Login';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/login' element={<Login/>}/>
         </Routes>
       </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
