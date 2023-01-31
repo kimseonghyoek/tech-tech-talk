676b132c1a55af9cf37f7d3c9665898f4cc43984
Kimseonghyoek <cilo1223@naver.com>
2023-02-01 03:35:14 +0900

    Separate styled-component

--- front/src/pages/Signup.tsx
+++ front/src/pages/Signup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginForm from '../components/LoginForm';
+import LoginForm from '../components/loginform/LoginForm';
 import styled from 'styled-components';
 
 const Container = styled.div`
