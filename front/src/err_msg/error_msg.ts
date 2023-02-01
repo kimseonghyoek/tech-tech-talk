90a50de5830a0dfe5dd958e4c09105530fb1f8a4
Kimseonghyoek <cilo1223@naver.com>
2023-02-01 23:00:32 +0900

    Update error message

--- front/src/err_msg/error_msg.ts
+++ front/src/err_msg/error_msg.ts
@@ -1,4 +1,9 @@
 export default {
   WRONG_NAME_FORMAT: "닉네임 형식에 맞지 않습니다.",
-  EMPTY_NAME_SPACE: "닉네임이 공백입니다."
+  EMPTY_NAME_SPACE: "닉네임이 공백입니다.",
+  WRONG_EMAIL_FORMAT: "이메일 형식에 맞지 않습니다.",
+  EMPTY_EMAIL_SPACE: "이메일이 공백입니다.",
+  WRONG_PW_FORMAT: "비밀번호 형식에 맞지 않습니다.",
+  EMPTY_PW_SPACE: "비밀번호가 공백입니다.",
+  FAIL_MATCH_PW: "비밀번호 재확인에 실패하셨습니다.",
 }
