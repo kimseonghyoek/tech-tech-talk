3c1dfdd671f8824dc70074674578fa38d588ab2d
Kimseonghyoek <cilo1223@naver.com>
2023-02-01 22:57:47 +0900

    Update Loginform component

--- front/src/components/loginform/LoginForm.tsx
+++ front/src/components/loginform/LoginForm.tsx
@@ -2,21 +2,23 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import palette from "../../palette";
-import Input from "../Input";
+import Input from "../input/Input";
 import Button from "../Button";
 import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
 import { error } from "console";
 import { Container } from "./style";
+import err_msg from '../../err_msg/error_msg';
+import InputMsg from "../input/InputMsg";
 
 const public_url = process.env.PUBLIC_URL;
 
 function LoginForm(): JSX.Element {
   const location = useLocation();
 
-  const [userName, setName] = useState<string>("");
-  const [userEmail, setEmail] = useState<string>("");
-  const [userPw, setPw] = useState<string>("");
+  const [userName, setName] = useState<string>(" ");
+  const [userEmail, setEmail] = useState<string>(" ");
+  const [userPw, setPw] = useState<string>(" ");
   const [rePw, setRepw] = useState<string>("");
   const [check, setCheck] = useState<boolean>(false);
 
@@ -32,39 +34,42 @@ function LoginForm(): JSX.Element {
   const changeName = (e: React.ChangeEvent<HTMLInputElement>): any => {
     setName(e.target.value);
   };
-  const checkName = () => {
+  const checkName = (): string => {
     const name_regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
     if (userName.match(name_regExp) || userName.length >= 21) {
-      alert("닉네임 형식에 맞지 않습니다.");
+      return err_msg.WRONG_NAME_FORMAT;
     } else if (userName === "") {
-      alert("닉네임이 공백입니다.");
+      return err_msg.EMPTY_NAME_SPACE;
     }
+    return "";
   }
 
   const changeEmail = (e: React.ChangeEvent<HTMLInputElement>): any => {
     setEmail(e.target.value);
   };
-  const checkEmail = () => {
+  const checkEmail = (): string => {
     const email_regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     if(userEmail.match(email_regExp) || userEmail.length >= 51) {
-      alert("이메일 형식에 맞지 않습니다.");
+      return err_msg.WRONG_EMAIL_FORMAT
     } else if (userEmail === "") {
-      alert("이메일이 공백입니다.");
+      return err_msg.EMPTY_EMAIL_SPACE;
     }
+    return ""
   }
 
   const changePw = (e: React.ChangeEvent<HTMLInputElement>): any => {
     setPw(e.target.value);
   };
-  const checkPw = () => {
+  const checkPw = (): string => {
     const pw_regExp = /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
     if(!userPw.match(pw_regExp) || userPw.length >= 30) {
-      alert("비밀번호 형식에 맞지 않습니다.");
+      return err_msg.WRONG_PW_FORMAT;
     } else if (userPw === "") {
-      alert("비밀번호가 공백입니다.");
+      return err_msg.EMPTY_PW_SPACE;
     } else if (userPw !== rePw) {
-      alert("비밀번호 재확인에 실패하셨습니다.");
+      return err_msg.FAIL_MATCH_PW;
     }
+    return "";
   }
 
   const changeRePw = (e: React.ChangeEvent<HTMLInputElement>): any => {
@@ -75,7 +80,9 @@ function LoginForm(): JSX.Element {
     e.preventDefault();
     checkName();
     checkEmail();
-    checkPw()
+    checkPw();
+
+    
   };
 
   const submitLogin = (e: React.ChangeEvent<HTMLFormElement>): any => {
@@ -133,21 +140,24 @@ function LoginForm(): JSX.Element {
           <div className="inputs">
             <p>닉네임 (특수문자 제외 20자)</p>
             <Input placeholder="" onChange={changeName} />
+            <InputMsg msg={checkName()}/>
           </div>
           <div className="inputs">
             <p>이메일 (이메일 주소)</p>
             <Input placeholder="" onChange={changeEmail} />
+            <InputMsg msg={checkEmail()}/>
           </div>
           <div className="inputs">
             <p>비밀번호 (특수문자 포함 8자 이상)</p>
-            <Input placeholder="" onChange={changePw} />
+            <Input placeholder="" onChange={changePw} type="password" />
+            <InputMsg msg={checkPw()}/>
           </div>
           <div className="inputs">
             <p>비밀번호 재확인</p>
-            <Input placeholder="" onChange={changeRePw} />
+            <Input placeholder="" onChange={changeRePw} type="password"/>
           </div>
           <div className="check-sign">
-            <input type="checkbox" checked={check} onChange={checkSignd} />
+            <input type="checkbox" checked={check} onChange={checkSignd}/>
             <p> Tech-Tech-Talk(이하 텍텍톡) 회원가입에 동의하십니까?</p>
           </div>
           <Button disabled={!check} id="login-btn" text="회원가입" />
