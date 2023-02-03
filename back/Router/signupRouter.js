1971f130a9a384f434f4d25a5feb068c0ffaf2c2
Kimseonghyoek <cilo1223@naver.com>
2023-02-03 16:38:37 +0900

    Hashed user password

--- back/Router/signupRouter.js
+++ back/Router/signupRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 router.post("/post", async (req, res) => {  
-  console.log(req.body);
+  const data = req.body.data;
+  const salt = 12;
+  const hashed = await bcrypt.hash(data.userPw, salt);
+  console.log(hashed);
+  res.send("잘받음");
 });
 
 module.exports = router;
