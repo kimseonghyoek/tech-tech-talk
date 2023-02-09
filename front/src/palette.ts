9305b8fc2b13895793510526ef92c2ea6891018e
Kimseonghyoek <cilo1223@naver.com>
2023-02-09 17:12:03 +0900

    Add color in palette

--- front/src/palette.ts
+++ front/src/palette.ts
@@ -1,4 +1,5 @@
 // 색상표
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   white: "#FFFFFF",
   black: "#000000",
@@ -8,4 +9,7 @@ export default {
   main_color4: "#EB6440",
   naver_color: "#00bf18",
   kakao_color: "#fae301",
+  pro_back: "#7286D3",
+  pro_color1: "#F9F871",
+  pro_color2: "#B6E37C",
 }
