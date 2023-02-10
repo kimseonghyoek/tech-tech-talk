3892dac04b9724ce689b914f518b5b4e3386fd16
Kimseonghyoek <cilo1223@naver.com>
2023-02-11 02:47:13 +0900

    Update express app

--- back/app.js
+++ back/app.js
@@ -4,7 +4,8 @@ const app = express();
 import cors from 'cors';
 import signupRouter from './Router/signupRouter.js';
 import LoginRouter from './Router/LoginRouter.js';
-import { createProxyMiddleware } from 'http-proxy-middleware';
+import passport from 'passport'
+import expressSession from 'express-session';
 
 const PORT = 8000;
 const __dirname = path.resolve();
@@ -12,7 +13,12 @@ const __dirname = path.resolve();
 app.set("view engine", "html");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../front/build")));
-app.use(cors({ credentials: true, origin: "http://localhost:3000"}))
+app.use(expressSession({
+  secret: "TEST"
+}));
+app.use(cors({ credentials: true, origin: "http://localhost:3000"}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.sendFile("index.html");
