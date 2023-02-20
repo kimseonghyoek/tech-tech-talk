52f5dca1010307dac67868b32262b25c1db7ff94
Kimseonghyoek <cilo1223@naver.com>
2023-02-20 23:34:32 +0900

    Update mobile landing page

--- front/src/pages/Landing/MobilePage.tsx
+++ front/src/pages/Landing/MobilePage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`
 
 function MobilePage(): JSX.Element {
   return (
-    <div>
-      <p>Mobile Landing page</p>
-    </div>
+    <Container>
+      <p>Mobile landing page</p>
+    </Container>
   );
 };
 
