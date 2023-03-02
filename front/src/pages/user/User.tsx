e6fa4d05cb465d0796b49784333ab523f66802bf
Kimseonghyoek <cilo1223@naver.com>
2023-03-02 20:32:37 +0900

    Update user page

--- front/src/pages/user/User.tsx
+++ front/src/pages/user/User.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
+import axios from "axios";
+import React from "react";
+import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
@@ -7,11 +8,25 @@ const Container = styled.div`
 `;
 
 function User(): JSX.Element {
+  const getUser = () => {
+    axios
+      .get("/user/get")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      
+  };
+
+  getUser();
+
   return (
     <Container>
       <p>User Page</p>
     </Container>
-  )
+  );
 }
 
 export default User;
