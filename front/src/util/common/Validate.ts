a18ed4c99dd7f5960e005381fe2c8eadb772e4cd
Kimseonghyoek <cilo1223@naver.com>
2023-03-24 23:55:22 +0900

    Create name validate

--- front/src/util/common/Validate.ts
+++ front/src/util/common/Validate.ts
@@ -1,5 +1,20 @@
-interface validate {
+/* eslint-disable no-useless-escape */
+import error_msg from "../../err_msg/error_msg";
 
+interface validate {
+  nameValidate: (name: string, post: boolean) => string | undefined;
 }
 
-const ValidateCommon: Object = {};
+export const ValidateCommon: validate = {
+  nameValidate: function(name: string, post: boolean) {
+    const name_regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
+    if(name.match(name_regExp) || name.length >= 51) {
+      post = false;
+      return error_msg.WRONG_NAME_FORMAT;
+    } else if (name === "") {
+      post = false;
+      return error_msg.EMPTY_NAME_SPACE;
+    }
+    return "";
+  },
+};
