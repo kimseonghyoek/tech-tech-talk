a06dc38f8079faf29d35993e1188a9186696f2ef
Kimseonghyoek <cilo1223@naver.com>
2023-03-31 21:58:30 +0900

    Uodate loginform component

--- front/src/components/loginform/LoginForm.tsx
+++ front/src/components/loginform/LoginForm.tsx
@@ -46,83 +46,21 @@ function LoginForm(): JSX.Element {
   const changeNickName = (e: React.ChangeEvent<HTMLInputElement>): any => {
     setNickName(e.target.value);
   };
-  const checkNickName = (): string => {
-    const name_regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
-    if (userNickName.match(name_regExp) || userNickName.length >= 51) {
-      post = false;
-      return err_msg.WRONG_NAME_FORMAT;
-    } else if (userNickName === "") {
-      return err_msg.EMPTY_NAME_SPACE;
-    }
-    return "";
-  };
 
   const changeNumber = (e: React.ChangeEvent<HTMLInputElement>): any => {
     setNumber(e.target.value);
-  };
-  const checkNumber = (): string => {
-    const number_regExp = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
-    if (!userNumber.match(number_regExp) || userNumber.length >= 12) {
-      post = false;
-      return err_msg.WRONG_NUMBER_FORMAT;
-    } else if (userNumber === "") {
-      return err_msg.EMPTY_NUMBER_SPACE;
-    }
-    return "";
   };
 
   const changeName = (e: React.ChangeEvent<HTMLInputElement>): any => {
     setName(e.target.value);
   };
-  const checkName = (): string => {
-    const name_regExp = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g;
-    if (userName.match(name_regExp) || userName.length >= 21) {
-      post = false;
-      return err_msg.WRONG_NICK_FORMAT;
-    } else if (userName === "") {
-      post = false;
-      return err_msg.EMPTY_NICK_SPACE;
-    }
-    return "";
-  };
 
   const changeEmail = (e: React.ChangeEvent<HTMLInputElement>): any => {
     setEmail(e.target.value);
   };
-  const checkEmail = (): string => {
-    const email_regExp =
-      /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-    if (!userEmail.match(email_regExp) || userEmail.length >= 51) {
-      post = false;
-      return err_msg.WRONG_EMAIL_FORMAT;
-    } else if (userEmail === "") {
-      post = false;
-      return err_msg.EMPTY_EMAIL_SPACE;
-    }
-    return "";
-  };
 
   const changePw = (e: React.ChangeEvent<HTMLInputElement>): any => {
     setPw(e.target.value);
-  };
-  const checkPw = (): string => {
-    const pw_regExp =
-      /^.*(?=^.{8,15}$)(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&+=]).*$/;
-    if (!userPw.match(pw_regExp) || userPw.length >= 30) {
-      post = false;
-      return err_msg.WRONG_PW_FORMAT;
-    } else if (userPw === "") {
-      post = false;
-      return err_msg.EMPTY_PW_SPACE;
-    }
-    return "";
-  };
-  const reCheckPw = (): string => {
-    if (userPw !== rePw) {
-      post = false;
-      return err_msg.FAIL_MATCH_PW;
-    }
-    return "";
   };
 
   const changeRePw = (e: React.ChangeEvent<HTMLInputElement>): any => {
@@ -131,9 +69,6 @@ function LoginForm(): JSX.Element {
 
   const submitSignup = (e: React.ChangeEvent<HTMLFormElement>): any => {
     e.preventDefault();
-    checkName();
-    checkEmail();
-    checkPw();
 
     if (post === true) {
       axios
@@ -159,8 +94,6 @@ function LoginForm(): JSX.Element {
 
   const submitLogin = (e: React.ChangeEvent<HTMLFormElement>): any => {
     e.preventDefault();
-    checkEmail();
-    checkPw();
 
     if (post === true) {
       axios
@@ -199,12 +132,12 @@ function LoginForm(): JSX.Element {
           <div className="inputs">
             <p>이메일 (이메일 주소)</p>
             <Input placeholder="" onChange={changeEmail} />
-            <InputMsg msg={checkEmail()} />
+            <InputMsg msg={ValidateCommon.emailValidate(userEmail, post)} />
           </div>
           <div className="inputs">
             <p>비밀번호 (영 대문자,소문자, 특수문자 포함 8자 이상)</p>
             <Input type="password" placeholder="" onChange={changePw} />
-            <InputMsg msg={checkPw()} />
+            <InputMsg msg={ValidateCommon.emailValidate(userPw, post)} />
           </div>
           <Button disabled={false} id="login-btn" text="로그인" />
         </form>
@@ -266,7 +199,7 @@ function LoginForm(): JSX.Element {
           <div className="inputs">
             <p>비밀번호 재확인</p>
             <Input placeholder="" onChange={changeRePw} type="password" />
-            <InputMsg msg={reCheckPw()} />
+            <InputMsg msg={ValidateCommon.reCheckPwValidate(userPw, rePw, post)} />
           </div>
           <div className="check-sign">
             <input type="checkbox" checked={check} onChange={checkSignd} />
