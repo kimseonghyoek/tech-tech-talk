f4aa0eea9453ca3b7a9d03609ff6d96e23bd8603
Kimseonghyoek <cilo1223@naver.com>
2023-04-03 17:02:14 +0900

    Create Styled component

---
+++ front/src/pages/Styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const style = styled.div`
+    display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  flex-direction: column;
+`;
+
+export default style;
