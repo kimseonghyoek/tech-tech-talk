f68e11b9d219b004ff60e4efa3f264c330359185
Kimseonghyoek <cilo1223@naver.com>
2023-04-03 17:04:31 +0900

    Create Contents layout

---
+++ front/src/components/contents/ContentsLayout.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const style = styled.div`
+  width: 65%;
+  height: 100vh;
+  background-color: #F0F0F0;
+  padding: 1rem;
+  display: flex; 
+  justify-content: center;
+`;
+
+export default style;
