6f1555ce81f6a7bb724295ea46670e023b221137
overloper <overloper0225@gmail.com>
2023-04-24 11:39:48 +0900

    Update index.js

--- back/passport/index.js
+++ back/passport/index.js
@@ -6,18 +6,19 @@ const Strategy  = require("passport-local").Strategy;
 module.exports = () => {
   passport.serializeUser((user, done) => {
     console.log("serialize User ", user[0].id);
-    done(null, user);
+    done(null, user[0].id);
   });
 
-  passport.deserializeUser((email, done) => {
-    console.log("deserializeUser email", email);
+  passport.deserializeUser((id, done) => {
+    console.log("deserializeUser id", id);
     let userInfo;
-    const sql = "SELECT * FROM user_table where email=?";
-    conn.query(sql, email, (err, result) => {
+    const sql = "SELECT * FROM user_table where id=?";
+    conn.query(sql, id, (err, result) => {
       if (err) {
         console.log("error");
         done(err);
       }
+      console.log(result[0].id);
       const json = JSON.stringify(result[0]);
       userInfo = JSON.parse(json);
       done(null, userInfo.id);
