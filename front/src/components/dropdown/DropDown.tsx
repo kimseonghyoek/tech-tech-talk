f6bb929d2b87e570c2c87a7b64fc92f9a9026260
overloper <overloper0225@gmail.com>
2023-04-24 10:13:36 +0900

    Update Dropdwon component

--- front/src/components/dropdown/DropDown.tsx
+++ front/src/components/dropdown/DropDown.tsx
@@ -1,13 +1,29 @@
 import react, { useState } from "react";
 import styled from "styled-components";
+import palette from "../../palette";
 
 export type DropProps = {
   title: string | null;
 };
 
 const Container = styled.article`
+  background-color: ${palette.main_color3};
+  width: fit-content;
+  padding: 0rem 2.2rem;
+  border-radius: 5px;
+
+  h3 {
+    color: ${palette.white};
+  }
+
   ul {
+    z-index: 1;
+    padding: 0%;
     list-style: none;
+  }
+
+  li {
+    padding: 0%;
   }
 `;
 
@@ -19,13 +35,11 @@ const DropDown = (props: DropProps) => {
   };
 
   return (
-    <Container>
-      <button onClick={ChangeVisible}>{props.title}</button>
-      {visibleItem ? (
-        <ul>
-          <li>test</li>
-        </ul>
-      ) : null}
+    <Container onMouseEnter={ChangeVisible} onMouseLeave={ChangeVisible}>
+      <h3> {props.title}</h3>
+      {
+        visibleItem ? (<p>asdfasdf</p>) : (null)
+      }
     </Container>
   );
 };
