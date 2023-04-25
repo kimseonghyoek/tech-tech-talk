e603af96f1a4aaa6a376c004b532cb2d708ac1fd
overloper <overloper0225@gmail.com>
2023-04-25 16:31:08 +0900

    Update recoil store

--- front/src/recoil/store.ts
+++ front/src/recoil/store.ts
@@ -1,6 +1,9 @@
 // Atom Store
 
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
 
 export const showItem = atom<boolean>({
   key: 'showitem',
@@ -9,5 +12,6 @@ export const showItem = atom<boolean>({
 
 export const checkLogin = atom<boolean>({
   key: 'login',
-  default: false
+  default: false,
+  effects_UNSTABLE: [persistAtom]
 });
