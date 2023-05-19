91dc51c21cac39c269a0e692ab077a0d974c95f7
overloper <overloper0225@gmail.com>
2023-05-19 22:29:25 +0900

    Update Styled

--- front/src/pages/Styled.ts
+++ front/src/pages/Styled.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 const style = styled.div`
-    display: flex;
+  display: flex;
   justify-content: center;
   align-items: center;
   flex: 1;
   flex-direction: column;
+  height: 100vh;
 `;
 
 export default style;
