3ebb3d96f6c45330d311d07cbc47e41c3cb21c4e
overloper <overloper0225@gmail.com>
2023-05-21 00:34:32 +0900

    Update PostCard components

--- front/src/components/PostCard.tsx
+++ front/src/components/PostCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Divider, List, Typography} from "antd";
+import styled from "styled-components";
 
 const data = [
     'Racing car sprays burning fuel into crowd.',
@@ -9,18 +10,27 @@ const data = [
     'Los Angeles battles huge wildfires.',
 ];
 
+const WrapCard = styled.div`
+  border-radius: 10px;
+  border: 0.07rem solid gray;
+`;
+
+const onClickEvent = (): any => {
+    return console.log('test');
+}
+
 function PostCard(): JSX.Element {
     return (
-        <>
+        <WrapCard>
             <Divider orientation="left">따끈따끈한 새 글</Divider>
             <List dataSource={data}
                   bordered
                   renderItem={(item) => (
-                      <List.Item>{item}
+                      <List.Item onClick={onClickEvent}>{item}
                       </List.Item>
                   )}
             />
-        </>
+        </WrapCard>
     )
 }
 
--- front/src/components/contents/ContentsLayout.ts
+++ front/src/components/contents/ContentsLayout.ts
@@ -3,11 +3,8 @@ import styled from "styled-components";
 const style = styled.div`
   width: 65%;
   height: 100vh;
-  background-color: #F0F0F0;
   padding: 1rem;
   margin: 2rem 0rem;
-  display: flex; 
-  justify-content: center;
 `;
 
 export default style;
