eb8f3aedc0ec38f7544cb8855660ef38d147b46a
overloper <overloper0225@gmail.com>
2023-05-23 20:01:37 +0900

    Create reducer folder

---
+++ front/src/reducer/user.ts
@@ -0,0 +1,30 @@
+export const initState = {
+    userData: {
+        email: "",
+        nickname: "",
+    }
+}
+
+export const LOG_IN_REQUEST: string = 'LOG_IN_REQUEST' as const;
+export const LOG_IN_SUCCESS: string = 'LOG_IN_SUCCESS' as const;
+export const LOG_IN_FAILURE: string = 'LOG_IN_FAILURE' as const;
+
+export const loginRequestAction = () => {
+    return {
+        type: LOG_IN_REQUEST,
+    }
+};
+
+export const loginSuccessAction = (data: object) => {
+    return {
+        type: LOG_IN_SUCCESS,
+        data
+    }
+}
+
+export const loginFailureAction = (data: object) => {
+    return {
+        type: LOG_IN_FAILURE,
+        data
+    }
+}
