83d1db1d45ff26cf00686dfa75595a7daa7d7d60
overloper <overloper0225@gmail.com>
2023-05-28 23:07:01 +0900

    Write json

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -3,6 +3,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 import requests
+import json
 
 html = urlopen("https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=230")
 bs_object  = BeautifulSoup(html, "html.parser")
@@ -14,13 +15,13 @@ for i in range(1, 11):
 
 title_list = []
 
-for link in news_link:
-    header = {"user-agent": "Mozilla/5.0"}
+with open('./Headline.json', 'a+', encoding='UTF-8') as make_file:
+    for link in news_link:
+        header = {"user-agent": "Mozilla/5.0"}
 
-    html = requests.get(link, headers=header)
-    soup = BeautifulSoup(html.text, 'html.parser')    
+        html = requests.get(link, headers=header)
+        soup = BeautifulSoup(html.text, 'html.parser')    
 
-    title = soup.find('h2', {'id': 'title_area'}).text
-    title_list.append(title)
-
-    print(title)
+        title = soup.find('h2', {'id': 'title_area'}).text
+        title_list.append(title)
+    json.dump(title_list, make_file, ensure_ascii=False)
