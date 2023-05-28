f2f7491907c1351cc44d089d3e69989cce16a755
overloper <overloper0225@gmail.com>
2023-05-28 20:12:03 +0900

    Update crawling sys

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -3,13 +3,12 @@ from bs4 import BeautifulSoup
 from collections import OrderedDict
 
 
-html = urlopen("https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105")
+html = urlopen("https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=230")
 bsObject  = BeautifulSoup(html, "html.parser")
-newsData = OrderedDict();
 
-newsList = bsObject.find('ul', class_ = "sh_list")
-newsItem = newsList.find('li', class_ = "sh_item")
+newsTitle = []
 
-headlineNews = bsObject.find('a', class_ = "sh_text_headline")
+for i in range(1, 11):
+    newsTitle.append(bsObject.select('#main_content > div.list_body.newsflash_body > ul.type06_headline > li:nth-child('+str(i)+') > dl > dt:nth-child(2) > a'))
 
-print(newsList)
+print(newsTitle)
