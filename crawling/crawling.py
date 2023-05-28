515b97c6887b8d1db34ea82c6ae58d0a12fedc57
overloper <overloper0225@gmail.com>
2023-05-28 22:41:56 +0900

    Update crawling system

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -1,14 +1,26 @@
+# 네이버 IT일반 뉴스 크롤링
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from collections import OrderedDict
-
+import requests
 
 html = urlopen("https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=230")
-bsObject  = BeautifulSoup(html, "html.parser")
+bs_object  = BeautifulSoup(html, "html.parser")
 
-newsTitle = []
+news_link = []
 
 for i in range(1, 11):
-    newsTitle.append(bsObject.select('#main_content > div.list_body.newsflash_body > ul.type06_headline > li:nth-child('+str(i)+') > dl > dt:nth-child(2) > a'))
+    news_link.append(bs_object.select('#main_content > div.list_body.newsflash_body > ul.type06_headline > li:nth-child('+str(i)+') > dl > dt:nth-child(2) > a')[0].attrs['href'])
 
-print(newsTitle)
+title_list = []
+
+for link in news_link:
+    header = {"user-agent": "Mozilla/5.0"}
+
+    html = requests.get(link, headers=header)
+    soup = BeautifulSoup(html.text, 'html.parser')    
+
+    title = soup.find('h2', {'id': 'title_area'}).text
+    title_list.append(title)
+
+    print(title)
