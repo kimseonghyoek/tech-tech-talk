911f83af97a747a4692189ae73da43e05dcf9119
overloper <overloper0225@gmail.com>
2023-05-30 23:08:55 +0900

    Update Crawling sys

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -7,32 +7,41 @@ import json
 from pathlib import Path
 import os
 
-html = urlopen("https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=105&sid2=230")
+html = urlopen("https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1=105")
 bs_object  = BeautifulSoup(html, "html.parser")
 dir_news_link = Path("./link.json")
 dir_title = Path("./HeadLine.json")
 
 
 news_link = []
-
-with open('./link.json', 'a+', encoding='UTF-8') as make_file:
-    for i in range(1, 11):
-        news_link.append(bs_object.select('#main_content > div.list_body.newsflash_body > ul.type06_headline > li:nth-child('+str(i)+') > dl > dt:nth-child(2) > a')[0].attrs['href'])
-    if dir_news_link.exists() :
-        os.remove('./link.json')
-    else: json.dump(news_link, make_file, ensure_ascii=False)     
-
 title_list = []
 
-with open('./Headline.json', 'a+', encoding='UTF-8') as make_file:
-    for link in news_link:
-        header = {"user-agent": "Mozilla/5.0"}
+def WriteLink():
+    with open('./link.json', 'a+', encoding='UTF-8') as make_file:
+        for i in range(1, 7):
+            news_link.append(bs_object.select('#main_content > div > div._persist > div.section_headline > ul > li:nth-child('+str(i)+') > div.sh_text > a')[0].attrs['href'])
+        json.dump(news_link, make_file, ensure_ascii=False)
 
-        html = requests.get(link, headers=header)
-        soup = BeautifulSoup(html.text, 'html.parser')    
+if dir_news_link.exists() :
+    os.remove('./link.json')
+    WriteLink()
+else :
+    WriteLink()
 
-        title = soup.find('h2', {'id': 'title_area'}).text
-        title_list.append(title)
-        if dir_title.exists() :
-            print('adsfasdf')
-    json.dump(title_list, make_file, ensure_ascii=False)
+def WriteHeadLine():
+    with open('./Headline.json', 'a+', encoding='UTF-8') as make_file:
+        for link in news_link:
+            header = {"user-agent": "Mozilla/5.0"}
+
+            html = requests.get(link, headers=header)
+            soup = BeautifulSoup(html.text, 'html.parser')    
+
+            title = soup.find('h2', {'id': 'title_area'}).text
+            title_list.append(title)
+        json.dump(title_list, make_file, ensure_ascii=False)
+
+if dir_title.exists() :
+    os.remove('./Headline.json')
+    WriteHeadLine()
+else :
+    WriteHeadLine()
