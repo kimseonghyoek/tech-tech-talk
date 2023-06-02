fd6dc9110ef2dca9dc51f41f43b8363d8db403e2
overloper <overloper0225@gmail.com>
2023-06-02 23:26:44 +0900

    Update community site

--- front/src/pages/comm/Community.tsx
+++ front/src/pages/comm/Community.tsx
@@ -2,34 +2,44 @@ import React from "react";
 import Container from "../Styled";
 import ContentsLayout from "../../components/contents/ContentsLayout";
 import PostCard from "../../components/postcards/PostCard";
-import {Layout, Menu, Slider} from "antd";
-import {Content} from "antd/es/layout/layout";
+import { Col, Layout, Menu, Row, Slider } from "antd";
+import { Content } from "antd/es/layout/layout";
 import Sider from "antd/es/layout/Sider";
-import {hover} from "@testing-library/user-event/dist/hover";
+import { hover } from "@testing-library/user-event/dist/hover";
 import palette from "../../palette";
 import SideBar from "../../components/SideBar";
-import headlineData from '../../Headline.json'
-import newsLink from '../../link.json';
-import NewsCard from "../../components/postcards/NewsCard";
 
 function Comm(): JSX.Element {
   return (
-    <Layout style={{ padding: "0rem 17rem", minWidth: "1200px/*display: flex;justify-content: center*/;"}}>
-        <Content style={{ backgroundColor: `${palette.white}`, padding: "1rem" }}>
-            <div style={{ display: "flex", flexDirection: "row"}}>
-                <PostCard title="실시간 핫 글"/>
-                <PostCard title="광고"/>
-            </div>
-                <PostCard title="이 주의 핫 글" />
-                <PostCard title="소프트웨어"/>
-                <PostCard title="하드웨어"/>
-        </Content>
-        <Sider width="350" style={{ backgroundColor: "white", width: "fit-content" }}>
-            <>
-                <SideBar/>
-                <NewsCard title="이슈된 IT 기사" datas={headlineData} link={newsLink.map((link) => { return link })}/>
-            </>
-        </Sider>
+    <Layout style={{ width: "1200px", margin: "auto" }}>
+      <Content style={{ backgroundColor: `${palette.white}`, padding: "1rem" }}>
+        <Row>
+          <Col xs={24} md={12}>
+            <PostCard title="실시간 핫 글" />
+          </Col>
+          <Col xs={24} md={12}> 
+            <PostCard title="광고" />
+          </Col>
+        </Row>
+        <PostCard title="이 주의 핫 글" />
+        <PostCard title="소프트웨어" />
+        <PostCard title="하드웨어" />
+      </Content>
+      <Sider
+        width="350"
+        style={{ backgroundColor: "white", width: "fit-content" }}
+      >
+        <>
+          <SideBar />
+          {/* <NewsCard
+            title="이슈된 IT 기사"
+            datas={headlineData}
+            link={newsLink.map((link) => {
+              return link;
+            })}
+          /> */}
+        </>
+      </Sider>
     </Layout>
   );
 }
