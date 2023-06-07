cc3f701e9f3ce395e2a7b82b1f53a94bc8d4ee3a
overloper <overloper0225@gmail.com>
2023-06-07 22:53:33 +0900

    Design: update header component

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -1,18 +1,21 @@
-import { Header } from 'antd/es/layout/layout';
-import React from 'react';
-import palette from '../../palette';
-import { Menu, MenuProps } from 'antd';
-import { HeaderData } from './HeaderItem';
-import { Link, useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import { Header } from "antd/es/layout/layout";
+import React, { useState } from "react";
+import palette from "../../palette";
+import { Button, Menu, MenuProps } from "antd";
+import { HeaderData } from "./HeaderItem";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { Desktop, Tablet } from "../../util/responsive";
+import { MenuFoldOutlined } from "@ant-design/icons";
 
-const item: MenuProps['items'] = HeaderData.map((element ,key) => ({
+const item: MenuProps["items"] = HeaderData.map((element, key) => ({
   key,
-  label: <>
-    <Link to={element.link}>{element.name}</Link>
-  </>
+  label: (
+    <>
+      <Link to={element.link}>{element.name}</Link>
+    </>
+  ),
 }));
-
 
 const StyledHeader = styled(Header)`
   display: flex;
@@ -27,23 +30,55 @@ const StyledHeader = styled(Header)`
 
   ul {
     background-color: ${palette.main_color1};
+
+    li {
+      span {
+        color: ${palette.main_color3};
+      }
+    }
+  }
+
+  .menu-items {
+    display: flex;
+    align-items: center;
+
+    button {
+      width: 10rem;
+      height: 4.5rem;
+      margin: 0rem 1rem;
+    }
+  }
+
+
+  #m-menu-bar {
+    font-size: 4.5rem;
   }
 `;
 
 function HeaderNav(): JSX.Element {
-
   const navi = useNavigate();
+
+  const [showItem, setShowItem] = useState(false);
 
   const naviToComm = () => {
     navi("/comm");
-  }
+  };
 
   return (
     <StyledHeader>
-        <img src={'/imgs/ttt.png'} alt='logo' onClick={naviToComm}/>
-        <Menu mode='horizontal' items={item} />
+      <img src={"/imgs/ttt.png"} alt="logo" onClick={naviToComm} />
+      <div className="menu-items">
+        <Desktop>
+        <Menu mode="horizontal" items={item} />
+        </Desktop>
+        <Tablet>
+          <MenuFoldOutlined onClick={(): any => {
+            return console.log("test")
+          }} rev id="m-menu-bar"/>
+        </Tablet>
+      </div>
     </StyledHeader>
-  )
+  );
 }
 
 export default HeaderNav;
