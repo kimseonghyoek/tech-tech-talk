dc2d21002dc5b4b1c45d0dd83e123a100f7f76d2
overloper <overloper0225@gmail.com>
2023-06-14 09:24:06 +0900

    Feat: Create write

---
+++ front/src/pages/Write.tsx
@@ -0,0 +1,17 @@
+import { Content } from 'antd/es/layout/layout';
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled(Content)`
+  
+`;
+
+function Write(): JSX.Element {
+  return (
+    <Container>
+      write
+    </Container>
+  )
+};
+
+export default Write;
