b4d10c25f56609f335793b96fa52959a96967457
overloper <overloper0225@gmail.com>
2023-06-14 22:42:09 +0900

    Feat: text editer

--- front/src/pages/Write.tsx
+++ front/src/pages/Write.tsx
@@ -1,17 +1,81 @@
-import { Content } from 'antd/es/layout/layout';
-import React from 'react';
-import styled from 'styled-components';
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Select, Upload } from "antd";
+import TextArea from "antd/es/input/TextArea";
+import { Content } from "antd/es/layout/layout";
+import { Option } from "antd/es/mentions";
+import React, { useMemo, useState } from "react";
+import styled from "styled-components";
+import palette from "../palette";
+import ReactQuill from "react-quill";
+import 'react-quill/dist/quill.snow.css';
 
 const Container = styled(Content)`
-  
+  width: 70vw;
+  height: 100%;
+  margin: 0 auto;
+  padding: 3rem;
+
+  p {
+    font-size: 1.4rem;
+  }
+
+  .ant-select {
+    width: 15rem;
+  }
+
+  input {
+    font-size: 1.8rem;
+  }
+
+
+  #submits {
+    text-align: right;
+    padding: 1.8rem;
+  }
+
+  #submits button {
+    width: 14rem;
+  }
+
+  .ql-editor {
+    min-height: 70vh;
+  }
 `;
 
 function Write(): JSX.Element {
+  const [value, setValue] = useState<string>('');
+
   return (
+    
     <Container>
-      write
+      <Form>
+        <Select defaultValue={"카테고리"}>
+          <Option value="소프트웨어">소프트웨어</Option>
+          <Option value="키보드">키보드</Option>
+          <Option value="마우스">마우스</Option>
+          <Option value="PC 부품">PC 부품</Option>
+          <Option value="그 외">그 외 제품</Option>
+        </Select>
+        <span>
+          <p>제목 입력란</p>
+          <Input placeholder="제목을 입력하세요." />
+        </span>
+        <span>
+          <p>사진</p>
+          <Upload>
+          <Button icon={<UploadOutlined rev={null}/>}>Upload</Button>
+          </Upload>
+        </span>
+        <span>
+          <p>내용</p>
+          <ReactQuill theme="snow" value={value} onChange={setValue}/>
+        </span>
+        <div id="submits">
+          <Button>등록하기</Button>
+        </div>
+      </Form>
     </Container>
-  )
-};
+  );
+}
 
 export default Write;
