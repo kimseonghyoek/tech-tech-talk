55b83ec72ab9f9b3dfa4949c893556acaddb4a6e
overloper <overloper0225@gmail.com>
2023-06-19 16:28:48 +0900

    Design: Add color

--- front/src/palette.ts
+++ front/src/palette.ts
@@ -8,6 +8,7 @@ export default {
   main_color2: "#D6E4E5",
   main_color3: "#497174",
   main_color4: "#EB6440",
+  face_color: "#3b5998",
   naver_color: "#00bf18",
   kakao_color: "#fae301",
   pro_back: "#7286D3",
