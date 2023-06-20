5c75d9b654694ec291bd8eb0671c5580d07714ae
overloper <overloper0225@gmail.com>
2023-06-20 20:49:43 +0900

    Updaste backend

--- back/Router/LoginRouter.js
+++ back/Router/LoginRouter.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post(
   "/post", 
+  isNotLoggedIn,
   (req, res, next) => {
     passport.authenticate(
       "local", {session: true},
--- back/app.js
+++ back/app.js
@@ -17,6 +17,7 @@ const PORT = 8000;
 
 app.set("view engine", "html");
 app.use(express.json());
+app.use(express.static("public"));
 app.use(cors({ credentials: true, origin: "localhost:3000" }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, "../front/build")));
