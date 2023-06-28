08acdfbdc3fa88c34dc065c74a17c95119a1e250
overloper <overloper0225@gmail.com>
2023-06-28 23:21:40 +0900

    Update Configure store

--- front/src/store/ConfigureStore.ts
+++ front/src/store/ConfigureStore.ts
@@ -1,7 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+// actions
+const LOG_IN = "LOG_IN";
+const LOG_OUT = "LOG_OUT";
+
+const initState = {
+    user: null,
+}
+
 const store = configureStore({
-    reducer: {}
+    reducer: {
+
+    }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
