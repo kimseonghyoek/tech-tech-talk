61c4c03d75f7df37740288f27f3b85545877ebdd
overloper <overloper0225@gmail.com>
2023-06-29 18:29:49 +0900

    Feat: Create checkItem state

--- front/src/store/ConfigureStore.ts
+++ front/src/store/ConfigureStore.ts
@@ -1,18 +1,29 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Action, configureStore } from "@reduxjs/toolkit";
 
 // actions
 const LOG_IN = "LOG_IN";
 const LOG_OUT = "LOG_OUT";
+const SHOW_ITEM = "SHOW_ITEM";
+const HIDE_ITEM = "HIDE_ITEM";
 
 const initState = {
     user: null,
+};
+
+const checkItem = (state: boolean = false, action: Action) => {
+    switch(action.type) {
+        case SHOW_ITEM:
+            return state = true;
+        case HIDE_ITEM:
+            return !state
+        default:
+            return state
+    };
 }
 
-const store = configureStore({
-    reducer: {
-
-    }
-});
+export const store = configureStore({
+    reducer: { checkItem }
+})
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
