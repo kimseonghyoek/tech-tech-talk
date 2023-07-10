c543c7b936cb89f5bd328ef0c1539e8e6e739ba5
overloper <overloper0225@gmail.com>
2023-07-10 22:41:55 +0900

    Update query

--- back/sql/post-table.sql
+++ back/sql/post-table.sql
@@ -1,10 +1,11 @@
 use talk;
 
-create table post_table (
-  id int not null AUTO_INCREMENT PRIMARY KEY,
-  FOREIGN KEY(nickname) REFERENCES user_table(nickname),
-  cate int not null,
-  title varchar(55) not null,
-  text varchar(10000) not null,
-  hash varchar(50) not null
+CREATE TABLE post_table (
+  	id int not null AUTO_INCREMENT PRIMARY KEY,
+  	nickname varchar(35) not null UNIQUE,
+  	cate int not null,
+  	title varchar(55) not null,
+  	text varchar(10000) not null,
+  	hash varchar(50) not null,
+  	FOREIGN KEY(nickname) REFERENCES user_table(nickname)
 );
