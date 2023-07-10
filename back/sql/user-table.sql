4c81f4b962406dbd83a3624fce9a6f38b41dec67
overloper <overloper0225@gmail.com>
2023-07-10 22:41:10 +0900

    Change query

--- back/sql/user-table.sql
+++ back/sql/user-table.sql
@@ -3,12 +3,13 @@ create database talk;
 use talk;
 
 create table user_table (
-    id int not null AUTO_INCREMENT PRIMARY KEY,
-    email varchar(50) not null PRIMARY KEY,
+    id int not null AUTO_INCREMENT,
+    email varchar(50) not null,
     name varchar(20) not null,
-    nickname varchar(35) not null PRIMARY KEY,
-    phonenum varchar(11) not null,
-    pw varchar(100) not null
+    nickname varchar(35) not null,
+    phone_num varchar(11) not null,
+    pw varchar(100) not null,
+    PRIMARY KEY(id, email, nickname)
 );
 
 -- user_table의 phonenum 컬럼의 이름을 phone_num으로 변경.
