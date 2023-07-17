1b9c31768f7bbe3bde65946cd9178d68a666dd0b
overloper <overloper0225@gmail.com>
2023-07-17 20:45:03 +0900

    Update: get response and show response data

--- front/src/components/Weather.tsx
+++ front/src/components/Weather.tsx
@@ -7,15 +7,20 @@ import axios, { AxiosResponse } from 'axios';
 
 const WrapContent = styled(Content)`
   display: flex;
+  flex-direction: column;
   border: 0.07rem solid ${palette.main_color3};
   border-radius: 5px;
+
+  #main-content {
+    padding: 1.2rem;
+  }
 `;
 
 const Weather = (): JSX.Element => {
 
   const WEATHER_API: any|string = process.env.REACT_APP_WEATHER;
 
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState(Object);
 
   const getWeatherAPI = () => {
     axios.get(WEATHER_API).then((response: AxiosResponse) => {
@@ -32,6 +37,12 @@ const Weather = (): JSX.Element => {
   return (
     <WrapContent>
       <Divider orientation='left'>날씨</Divider>
+      <div id='main-content'>
+        {
+          weather.name
+        },
+
+      </div>
     </WrapContent>
   )
 }
