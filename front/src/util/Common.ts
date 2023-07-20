f88d2f2ff4a17612d907d378a0a8a6b8d46ba79a
overloper <overloper0225@gmail.com>
2023-07-20 15:51:33 +0900

    Feat: getDay function

--- front/src/util/Common.ts
+++ front/src/util/Common.ts
@@ -5,6 +5,17 @@ export const Common: any = {
           case 'Seoul' :
             return location = "서울";
         }
-      }
-  }
+      },
+      
+  },
+
+  getDay: {
+    getDate: function(v: any) {
+      const date = new Date();
+      let month = date.getMonth() + 1;
+      let day = date.getDate();
+      return v = {month, day}
+    }
+  },
+
 };
