2602248777f1570b3642cb8793f0446f4738887b
overloper <overloper0225@gmail.com>
2023-08-08 18:05:27 +0900

    Update app.js

--- back/app.js
+++ back/app.js
@@ -4,10 +4,10 @@ const app = express();
 const cors = require("cors");
 const signupRouter = require("./Router/signupRouter");
 const loginRouter = require("./Router/LoginRouter");
-const manageRouter = require("./Router/manageRouter");
 const userRouter = require("./Router/userRouter");
 const writeRouter = require("./Router/WriteRouter");
 const commRouter = require("./Router/CommRouter");
+const postRouter = require("./Router/PostRouter");
 const passportConfig = require("./passport/index");
 const passport = require("passport");
 const expressSession = require("express-session");
@@ -18,7 +18,7 @@ const PORT = 8000;
 app.set("view engine", "html");
 app.use(express.json());
 app.use(express.static("public"));
-app.use(cors({ credentials: true, origin: "localhost:3000" }));
+// app.use(cors({ credentials: true, origin: "localhost:3000" }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, "../front/build")));
 passportConfig();
@@ -33,16 +33,18 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+// router
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
-
 app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
 app.use("/user", userRouter);
 app.use("/comm", commRouter);
 app.use("/write", writeRouter);
-
+app.use("/post", postRouter);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../front/build/index.html"));
 });
