3c3e5c297b63c2d16ddf48afef13cd1487ab6721
overloper <overloper0225@gmail.com>
2023-08-25 23:24:16 +0900

    Feat: SQL

--- back/Model/Post.js
+++ back/Model/Post.js
@@ -1,10 +1,39 @@
+const { resolve } = require("path");
+const conn = require("../DB/db");
+
 module.exports = {
-  createPost: (postDto) => {
+  createPost: (postDto, id) => {
     console.log("createPost 함수 탐");
     const title = postDto.title;
     const value = postDto.value;
     const cate = postDto.category;
     const tags = postDto.tags;
-    console.log(tags);
+
+    return new Promise((resolve, reject) => {
+      conn.getConnection((err, con) => {
+        if (err) {
+          throw err;
+        } else {
+          const user_id = id;
+          console.log(`-=-=-${user_id}`);
+
+          con.query(
+            `INSERT INTO post_table (title, text, cate, hash) VALUES ?,?,?,?`,
+            title, value, cate, tags,
+            (err, register) => {
+              if (err) {
+                console.log("Register Post Error : ", err);
+                reject();
+              };
+              if (register) {
+                console.log("게시물 등록 완료..?");
+                resolve(register);
+              }
+            }
+          )
+        }
+        con.release();
+      });
+    })
   }
 }
