0d9f0d0e2804d8d6a40b947e75915ab0bd97f17d
overloper <overloper0225@gmail.com>
2023-09-08 08:15:25 +0900

    Style: typo action type

--- front/src/actions/ActionTypes.ts
+++ front/src/actions/ActionTypes.ts
@@ -1,3 +1,3 @@
-export const LOG_IN_REQUEST: string = "LOG_IM_REQUST";
+export const LOG_IN_REQUEST: string = "LOG_IN_REQUST";
 export const LOG_IN_SUCCESS: string = "LOG_IN_SUCCESS";
 export const LOG_IN_FAILURE: string = "LOG_IN_FAILURE";
