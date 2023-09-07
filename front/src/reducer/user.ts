0846144c9a2edfd06c468feafd756d958a11d9bb
overloper <overloper0225@gmail.com>
2023-09-07 09:29:09 +0900

    Feat: User reducer

--- front/src/reducer/user.ts
+++ front/src/reducer/user.ts
@@ -1,33 +1,31 @@
-import { Action } from 'redux';
-import * as types from '../actions/ActionTypes';
+import { Action } from "redux";
+import * as types from "../actions/ActionTypes";
 
 // 초기상태 선언
 export const initialState = {
-  user: {
-    isLoggedIn: false
-  },
-}
+  user: null,
+  loginRequest: false,
+  loginError: false,
+  loginSuccess: false,
+};
 
-export const isLogin = {
-  type: types.IS_LOGGED_IN
-}
-
-export const isLogout = {
-  type: types.IS_LOGGED_OUT
-}
-
-const user = (state = initialState.user, action: Action) => {
+const user = (state = initialState, action: Action) => {
   switch (action.type) {
-    case types.IS_LOGGED_IN:
-      return {
-        isLoggedIn: true
-      }
-    case types.IS_LOGGED_OUT:
-      return {
-        isLoggedIn: false
-      }
-    default:
-      return state
+    case types.LOG_IN_REQUEST:
+      state.loginRequest = true;
+      state.loginError = false;
+      state.loginSuccess = false;
+      break;
+    case types.LOG_IN_SUCCESS:
+      state.loginRequest=false;
+      state.loginSuccess=true;
+      state.loginError=false;
+      break;
+    case types.LOG_IN_FAILURE:
+      state.loginRequest=false;
+      state.loginSuccess=false;
+      state.loginError=true;
+      break;
   }
 };
 
