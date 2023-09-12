4bc2039e143c1a43a44f947664bd9bf450b18fac
overloper <overloper0225@gmail.com>
2023-09-12 19:50:56 +0900

    Refactor: Apply redux

--- front/src/actions/ActionTypes.ts
+++
@@ -1,3 +0,0 @@
-export const LOG_IN_REQUEST = "LOG_IN_REQUST" as const;
-export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS" as const;
-export const LOG_IN_FAILURE = "LOG_IN_FAILURE" as const;
--- front/src/util/Combine.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { user } from '../reducer/user';
-
-const rootReducer = combineReducers({user});
-
-export default rootReducer;
