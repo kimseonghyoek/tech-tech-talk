a6d1f5d0d90eaf4cd4ec3756664dd2ec23a182eb
overloper <overloper0225@gmail.com>
2023-09-13 11:26:38 +0900

    Feat: Develop signup logic

--- back/Router/UserRouter.js
+++ back/Router/UserRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { isLoggedIn } = require("./middlewares");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const UserService = require("../Service/User");
 
 // user 로그인 여부 확인
 router.get("/", isLoggedIn, async (req, res, next) => {
@@ -23,6 +25,11 @@ router.get("/:id", async (req, res, next) => {
 // 회원가입
 router.post("/signup", async (req, res, next) => {
   try {
+    const { userName, userEmail, userPw, userNickname } = req.body.data;
+    const salt = 12;
+    const hashed = await bcrypt.hash(userPw, salt);
+    const userObject = { userEmail, hashed, userNickname, userName };
+    UserService.Signup(userObject);
   } catch (err) {
     console.error(err);
     next(err);
@@ -40,6 +47,24 @@ router.post("/login", async (req, res, next) => {
 
 // 유저 정보 수정
 router.patch("/:id", async (req, res, next) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+// 회원 탈퇴
+router.delete("/:id", async (req, res, next) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+// 로그아웃
+router.post("/logout", async (req, res, next) => {
   try {
   } catch (err) {
     console.error(err);
