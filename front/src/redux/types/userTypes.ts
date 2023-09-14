095b96559226542014cbe65f930a12a41466e7e4
overloper <overloper0225@gmail.com>
2023-09-14 10:48:07 +0900

    Feat: Type related to user

---
+++ front/src/redux/types/userTypes.ts
@@ -0,0 +1,25 @@
+export type userState = {
+  user: Object,
+  loginRequest: boolean,
+  loginError: boolean,
+  loginSuccess: boolean
+};
+
+export type signUpData = {
+  email: string,
+  pw: string,
+  nickname: string,
+  name: string,
+  phone_num: number
+};
+
+export type loginData = {
+  email: string,
+  pw: string
+}
+
+export interface checkUserSignUp {
+  code: string;
+  message: string;
+  payload: any;
+};
