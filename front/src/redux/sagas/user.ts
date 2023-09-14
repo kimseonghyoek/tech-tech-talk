10587058871083adb9ca05d69dc65dbb1301678b
overloper <overloper0225@gmail.com>
2023-09-14 18:44:30 +0900

    Feat: Create generator function

--- front/src/redux/sagas/user.ts
+++ front/src/redux/sagas/user.ts
@@ -1,21 +1,56 @@
 import axios from "axios";
+import {
+  checkUserSignUp,
+  loginData,
+  signUpData,
+  userIdData,
+} from "../types/userTypes";
+import { Action } from "redux";
 
-function signUpAPI(data: any) {
-  return axios.post("/signup", data);
-};
+function signUpAPI(data: signUpData) {
+  return axios.post<checkUserSignUp>("/signup", data);
+}
 
-function loginAPI(data: any) {
+function* signUp(action: Action) {
+  try {
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function loginAPI(data: loginData) {
   return axios.post("/login", data);
-};
+}
 
-function changeUserInfoAPI(data: any) {
+function* login(action: Action) {
+  try {
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function changeUserInfoAPI(data: userIdData) {
   return axios.patch(`user_id/nickname`, data);
-};
+}
+
+function* changeUSerInfo(action: Action) {
+  try {
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 function deleteUserAPI(data: any) {
   return axios.delete(`usaer_id`, data);
-};
+}
+
+function* deleteUser(action: Action) {
+  try {
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 function getUserAPI(data: any) {
   return axios.get(`user_id`, data);
-};
+}
