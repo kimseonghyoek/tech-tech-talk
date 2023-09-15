c22fe9607d906a23c5bae76c6fbea686a680ca44
overloper <overloper0225@gmail.com>
2023-09-15 14:48:34 +0900

    Feat: Create signup action

--- front/src/redux/actions/ActionTypes.ts
+++ front/src/redux/actions/ActionTypes.ts
@@ -1,3 +1,6 @@
 export const LOG_IN_REQUEST = "LOG_IN_REQUST" as const;
 export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS" as const;
 export const LOG_IN_FAILURE = "LOG_IN_FAILURE" as const;
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST" as const;
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS" as const;
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE" as const;
