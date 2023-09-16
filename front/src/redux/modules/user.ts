0e6e6780f5c9df3b57a7a0b43d8b8490cf69beae
overloper <overloper0225@gmail.com>
2023-09-16 23:37:26 +0900

    Feat: Update user module

--- front/src/redux/modules/user.ts
+++ front/src/redux/modules/user.ts
@@ -1,7 +1,35 @@
+import { Action } from 'redux';
+/* interface */
+interface signupUserData {
+  email: string,
+  pw: string,
+  nickname: string,
+  name: string,
+  phone_num: number
+};
+
 /* Action Type */
+const SIGN_UP_REQUEST = "user/SIGN_UP_REQUEST" as const;
 
 /* Action Create Type */
+export const signUp = (data: signupUserData) => ({
+  type: SIGN_UP_REQUEST,
+  data: data
+});
 
 /* Inital state of the module */
+const initialState: object = {
+  user: []
+};
 
 /* reducer */
+export default function userReducer(state = initialState, action: Action) {
+  switch (action.type) {
+    case SIGN_UP_REQUEST:
+      return {
+        ...state
+      }
+      default:
+        return state;
+  }
+}
