32a456a5b1e85c958b531873da86927a50c503a1
overloper <overloper0225@gmail.com>
2023-09-24 03:45:32 +0900

    refactor: Change store application file

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -33,46 +33,42 @@ function App() {
   const my_github_link = "https://github.com/kimseonghyoek/";
   return (
     <div className="App">
-      <CookiesProvider>
-        <Provider store={store}>
-          {/* <PersistGate loading={null} persistor={persistor}> */}
-            <BrowserRouter>
-              <Header />
-              <Routes>
-                <Route path="/" element={<Promotion />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/user" element={<User />} />
-                <Route path="/comm" element={<Comm />} />
-                <Route path="/comm/softwares" element={<Software />} />
-                <Route path="/comm/keyboard" element={<KeyBoard />} />
-                <Route path="/comm/mouse" element={<Mouse />} />
-                <Route path="/comm/pc" element={<PC />} />
-                <Route path="/comm/other" element={<Other />} />
-                <Route path="/used" element={<UsedMarket />} />
-                <Route path="/post" element={<Write />} />
-                <Route path="/test" element={<Test />} />
-                <Route path="/userinfo" element={<UserInfo />} />
-                <Route path="/setting" element={<Setting />} />
-                <Route path="/manage" element={<DevManage />} />
-                <Route path="/*" element={<NotFound />} />
-              </Routes>
-              <Footer
-                style={{
-                  textAlign: "center",
-                  padding: "2rem 5rem",
-                  backgroundColor: palette.main_color1,
-                }}
-              >
-                <h3>Tech-Tech-Talk _ Dev 2023 ~ </h3>
-                <a href={my_github_link}>
-                  <p>- Develop & Design by Overloper</p>
-                </a>
-              </Footer>
-            </BrowserRouter>
-          {/* </PersistGate> */}
-        </Provider>
-      </CookiesProvider>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Promotion />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/comm" element={<Comm />} />
+          <Route path="/comm/softwares" element={<Software />} />
+          <Route path="/comm/keyboard" element={<KeyBoard />} />
+          <Route path="/comm/mouse" element={<Mouse />} />
+          <Route path="/comm/pc" element={<PC />} />
+          <Route path="/comm/other" element={<Other />} />
+          <Route path="/used" element={<UsedMarket />} />
+          <Route path="/post" element={<Write />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/userinfo" element={<UserInfo />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/manage" element={<DevManage />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer
+          style={{
+            textAlign: "center",
+            padding: "2rem 5rem",
+            backgroundColor: palette.main_color1,
+          }}
+        >
+          <h3>Tech-Tech-Talk _ Dev 2023 ~ </h3>
+          <a href={my_github_link}>
+            <p>- Develop & Design by Overloper</p>
+          </a>
+        </Footer>
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </div>
   );
 }
