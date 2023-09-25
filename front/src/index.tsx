c4a778d81ca5463058c061638137fda23dca2965
overloper <overloper0225@gmail.com>
2023-09-25 22:59:51 +0900

    Feat: Apply redux store and redux-middleware

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,12 +4,22 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import configureStores from './redux/ConfigureStore';
+import createSagaMiddleware from '@redux-saga/core';
+import rootReducer, { rootSaga } from './redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const store = configureStores();
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+  reducer: rootReducer, 
+  middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(rootSaga);
 
 root.render(
   <React.StrictMode>
