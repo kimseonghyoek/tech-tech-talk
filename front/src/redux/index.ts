253681f24ed5821818c28465460b5c55d1625b28
overloper <overloper0225@gmail.com>
2023-09-25 22:59:18 +0900

    Feat: fork rootsagas

--- front/src/redux/index.ts
+++ front/src/redux/index.ts
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import user, { userSaga } from "./modules/user";
-import { all } from "redux-saga/effects";
+import { all, fork } from "redux-saga/effects";
 
 const rootReducer = combineReducers({
   user
@@ -10,5 +10,5 @@ export default rootReducer;
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga() {
-  yield all([userSaga]);
+  yield all([fork(userSaga)]);
 }
