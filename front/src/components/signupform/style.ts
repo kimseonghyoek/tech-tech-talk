d93a8ce0b395259963d23e42203a1808ebbb407e
overloper <overloper0225@gmail.com>
2023-09-29 14:17:51 +0900

    Design: create signup form style sheet

---
+++ front/src/components/signupform/style.ts
@@ -0,0 +1,110 @@
+import { Layout } from "antd";
+import styled from "styled-components";
+import palette from "../../palette";
+
+export const Container = styled(Layout)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 15px;
+  flex-direction: column;
+  background-color: transparent;
+
+  .logo-layout {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 3.5rem;
+
+    img {
+      border-radius: 25%;
+      width: 66px;
+      height: 71px;
+    }
+
+    h1 {
+      padding-left: 20px;
+      font-size: 2.8rem;
+    }
+  }
+
+  form {
+    flex: 1 1 auto;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    .ant-row {
+      justify-content: center;
+    }
+
+    .inputs {
+      width: 100%;
+      margin: 1rem;
+      p {
+        font-size: 1.4rem;
+        margin: 1rem 0rem;
+      }
+
+      input {
+        height: 4.2rem;
+      }
+    }
+
+    #login-btn {
+      margin: 2rem;
+      width: 100%;
+      height: 6rem;
+      background-color: ${palette.main_color3};
+      color: ${palette.white};
+    }
+  }
+
+  .login-info {
+    width: 87%;
+    display: flex;
+    justify-content: flex-end;
+    margin: 0rem 4rem;
+    text-align: right;
+    a {
+      text-decoration: none;
+      p {
+        font-weight: lighter;
+        color: ${palette.black};
+        margin: 0;
+        font-size: 1.3rem;
+      }
+    }
+  }
+
+  hr {
+    margin: 3rem;
+    width: 34.5rem;
+  }
+
+  .social-login {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .check-sign {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    input {
+    width: 1.7rem;
+      height: 1.7rem;
+    }
+
+    p {
+      padding: 0rem 0.9rem;
+      font-size: 1.3rem;
+    }
+  }
+`;
