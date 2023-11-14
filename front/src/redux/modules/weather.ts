a6b7a204ad310ca5bfacb378d9bcde43376c6cf0
overloper <overloper0225@gmail.com>
2023-11-14 23:05:37 +0900

    feat: create weather module

---
+++ front/src/redux/modules/weather.ts
@@ -0,0 +1,22 @@
+// state interface
+interface Istate {
+  
+};
+
+// Action Type
+const prefix: string = "weather/";
+export const GET_WEATHER_REQUEST = `${prefix}/GET_WEATHER_REQUEST` as const;
+export const GET_WEATHER_SUCCESS = `${prefix}/GET_WEATHER_SUCCESS` as const;
+export const GET_WEATHER_FAILURE = `${prefix}/GET_WEATHER_FAILURE` as const;
+
+// Action Create Type
+
+// initializing state
+
+// reducer
+
+// api address
+
+// saga functions
+
+// export weathersaga
