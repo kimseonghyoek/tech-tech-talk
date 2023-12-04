9c3aaef5f0e905896687096eaeb2d34fac2cb9dc
overloper <overloper0225@gmail.com>
2023-12-05 00:12:44 +0900

    design: update color opacity

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -21,11 +21,10 @@ const StyledHeader = styled(Header)`
   display: flex;
   align-items: center;
   padding: 0rem 3.5rem;
-  background-color: ${palette.main_color1};
+  background-color: rgba(239, 245, 245, 0.9);
   justify-content: space-between;
   top: 0;
   position: sticky;
-  opacity: 0.5;
 
   img {
     width: 9rem;
