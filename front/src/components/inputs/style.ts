4223155d611c838616635d248d66fbd2ae90abe1
overloper <overloper0225@gmail.com>
2024-01-17 20:51:39 +0900

    design: update design form.item

--- front/src/components/inputs/style.ts
+++ front/src/components/inputs/style.ts
@@ -2,17 +2,10 @@ import { Form } from "antd";
 import styled from "styled-components";
 
 export const Container = styled(Form.Item)`
-  display: flex;
-  flex-direction: column;
   width: 100%;
   margin: 1rem;
-  
-  p {
-    font-size: 1.4rem;
-    margin: 1rem 0rem;
-  }
 
-  input {
-    height: 4.2rem;
+  label {
+    font-size: 1.2rem !important;
   }
 `;
